e252553fec082d9e2e4e5fce41e6a3cbc020ec02
Jake Williamson <williamsonj0403@gmail.com>
2024-10-01 19:58:29 -0400

    set variables to call cookies, amounts of ingredients, and a variable to reference given amount from word problem (48 cookies) then calculate ingredients for any given number of cookies in a batch using same recipe

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -1,12 +1,24 @@
 //Terrence Williamson
 //09.28.24
 //Chapter 3 Programming Project
-//"Branch name"
+//Project-1-Ingredient-Adjuster
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+double cookies, cups, cupb, cupf, multi;
+//for a batch of 48 cookies, you need 1.5 cups of sugar, 1 cup of butter,
+//2.75 cups of flour; these are the numbers used in calc's below
+
 int main()
-{
-    cout << "Hello World!\n";
+{//display data to screen after user gets cookie total
+    cout << "How many cookies do you need to bake? ";
+    cin >> cookies;
+    multi = (cookies / 48);
+    cups = multi * 1.5, cupb = multi * 1, cupf = multi * 2.75;
+    cout << "For " << cookies << " cookies, you will need:\n* " << setprecision(2)
+        << fixed << cups << " cups of sugar\n* " << setprecision(2) << fixed <<
+    cupb << " cups of butter\n* " << setprecision(2) << fixed << cupf << " cups of flour";
+    return 0;
 }
