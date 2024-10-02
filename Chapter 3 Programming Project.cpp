961ee9c354bdb6a39d1edf3e13e25ec5735fab53
Jake Williamson <williamsonj0403@gmail.com>
2024-10-01 20:26:45 -0400

    set variable of math solution of two random numbers added together. display problem on screen, implement cin.get to force program to need one press of return key before showing answer

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -1,12 +1,21 @@
 //Terrence Williamson
-//09.28.24
+//10.01.24
 //Chapter 3 Programming Project
-//"Branch name"
+//Program-2-Math-Tutor
 
 #include <iostream>
 using namespace std;
+//random numbers used in this program for simple arithmetic
+int solution = 388 + 125;
+int b;
 
 int main()
-{
-    cout << "Hello World!\n";
+{//display problem on screen, then use cin.get to require
+ //enter key pressed before showing solution
+    cout << "Solve the equation below. To check your work once done, please press "
+        "the Enter or Return key\n\n";
+    cout << "    388\n+   125\n________";
+    cin.get();
+    cout << "    " << solution << "\n\n";
+    return 0;
 }
